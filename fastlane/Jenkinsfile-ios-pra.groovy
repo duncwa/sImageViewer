10b2105b25c3abbbe36f416695fcce8354dabceb
cicdbuild <cicdbuild@cicdbuilds-Mac-mini.local>
2020-08-22 23:54:14 -0700

    Add setup stage to jenkinsfile

--- fastlane/Jenkinsfile-ios-pra.groovy
+++ fastlane/Jenkinsfile-ios-pra.groovy
@@ -2,10 +2,21 @@ pipeline {
     agent any
 
     stages {
+      stage('Setup') {
+          steps {
+              echo 'Install Bundle Ruby Gems'
+              sh 'bundle install'
+          }
+      }
         stage('Build') {
             steps {
                 echo 'Test PRA'
                 sh 'bundle exec fastlane test_ios_pra'
+            }
+        }
+        stage('Inspect') {
+            steps {
+                echo 'Inspecting..'
             }
         }
         stage('Test') {
