90fd5d808c086ffcc9f14b6bee8df6a9c8dd2d14
Duncan Wallace <duncwa@duncwa.com>
2020-08-23 09:15:02 -0700

    Added sdlc jenkinsfiles and configured PRA

--- fastlane/Jenkinsfile-ios-pra.groovy
+++ fastlane/Jenkinsfile-ios-pra.groovy
@@ -1,5 +1,27 @@
+#!/usr/bin/env groovy
+
+//
+//  Jenkinsfile-ios-pra.groovy
+//  sImageViewer
+//  Created by Duncan Wallace 08/21/2020
+//  Copyright © 2020. Duncwa LLC.  All rights reserved
+
+
 pipeline {
-    agent any
+    agent { any }
+
+    options {
+      ansiColor("xterm")
+      timeout(time: 1, unit: "HOURS")
+      buildDiscarder(logRotator(numToKeepStr: "5", artifactNumToKeepStr: "1"))
+    }
+
+    environment {
+      ghprbPullId = "${env.PULL_REQ_NUM}"
+      BUILD_NUM = "${env.BUILD_ID}"
+      PR_NUM = "${env.PULL_REQ_NUM}"
+      PR_URL ="https://github.com/duncwa/sImageViewer/pull/${env.PULL_REQ_NUM}"
+    }
 
     stages {
       stage('Setup') {
@@ -11,26 +33,39 @@ pipeline {
               sh 'rvm list'
           }
       }
-        stage('Build') {
-            steps {
-                echo 'Test PRA'
-                sh 'bundle exec fastlane test_ios_pra'
-            }
+      stage('Build') {
+          steps {
+              echo 'Test PRA'
+              sh 'bundle exec fastlane test_ios_pra'
+          }
+          post {
+            always { stash includes: "fastlane/*_output/**/*", name: "test_ios_pra", allowEmpty: true }
+          }
+      }
+    }
+
+    post {
+      always {
+        script {
+          try { unstash "test_ios_pra" }  catch (e) { echo "Failed to unstash stash: " + e.toString() }
         }
-        stage('Inspect') {
-            steps {
-                echo 'Inspecting..'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
+        // sh "bundle exec fastlane danger"
+        archiveArtifacts artifacts: "Fastlane*_output/**/*", fingerprint: true
+      }
+
+      sucess {
+        sh "echo 'Build Successful' "
+      }
+
+      unstable {
+        sh "echo 'Build Unstable' "
+
+      }
+
+      failure {
+        sh "echo 'Build Failed' "
+
+      }
+
     }
 }
