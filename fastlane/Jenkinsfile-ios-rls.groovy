1e6f74222f3c1eaac4d11a41c22854336ceb0a61
Duncan Wallace <duncwa@gmail.com>
2022-02-24 20:14:43 -0800

    Fix Release Authentication Issue (#24) (#25)
    * Add fastlane session credential secret text
    
    * Add application specific password
    
    * Trigger build test
    
    * Setup to use new Apple API key for release with JWT
    
    * Update Gemfile and fastfile lanes
    
    * Remove username from slack action call in PRA and QE lanes
    
    * Add slack webhook url as a jenkins credential
    
    * Set to run with updated danger file
    
    * Add slack credential secret to Jenkinsfiles
    
    * Archive inspect output into build
    
    * Fix syntax on and rename for bulkextractor inspect
    
    * Add correct archive artifacts path fix
    
    * Correct lane names in jenkinsfile
    
    * Update MBP dangerfile
    
    Co-authored-by: Duncan Wallace <duncwa@duncwa.com>
    
    Co-authored-by: Duncan Wallace <duncwa@duncwa.com>

--- fastlane/Jenkinsfile-ios-rls.groovy
+++ fastlane/Jenkinsfile-ios-rls.groovy
@@ -17,7 +17,12 @@ pipeline {
 
     environment {
       FASTLANE_PASSWORD = credentials("appledw4workonly")
+      FASLANE_SESSION = credentials("s.applesession")
+      FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD = credentials("s.applicationspecific")
+      FASTLANE_APPLE_API = credentials("s.appleapikey")
+      FASTLANE_APPLE_APPSTORE_API = credentials("s.appleapikey_appstore")
       BUILD_NUM = "${env.BUILD_ID}"
+      SLACK_URL = credentials("s.slackwebhookurl")
       SLACK_CHANNEL = "${env.SLACK_CHANNEL}"
     }
 
